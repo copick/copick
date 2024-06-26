6b7f9778dccf563ed274ee58806091469fc9406e
uermel <uermel@czii.org>
2024-06-26 14:28:08 -0700

    update get index

--- src/copick/ops/index.py
+++ src/copick/ops/index.py
@@ -6,6 +6,12 @@ from copick.models import (
     CopickRun,
     CopickVoxelSpacing,
 )
+from copick.ops.get import (
+    get_meshes,
+    get_picks,
+    get_segmentations,
+    get_voxelspacings,
+)
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -22,34 +28,16 @@ def index_segmentations(
 
     logger.setLevel(log_level)
 
-    if isinstance(run, str):
-        run = root.get_run(name=run)
-        if run is None:
-            logger.error(f"Run {run} not found.")
-            return None
-
-    if voxel_spacings is None:
-        voxel_spacings = []
-    elif isinstance(voxel_spacings, float):
-        voxel_spacings = run.get_voxel_spacing(voxel_spacings)
-    elif isinstance(voxel_spacings, Iterable):
-        voxel_spacings = [run.get_voxel_spacing(spacing) for spacing in voxel_spacings]
-
-    segs = {}
-    for seg in run.segmentations:
-        seglist = segs.get(seg.voxel_size, [])
-        seglist.append(seg)
+    segmentations = get_segmentations(root, run, voxel_spacings, parallel=True)
+    segmentations = sorted(segmentations, key=lambda x: x.voxel_size)
 
     lines = []
-
-    for spacing, seglist in segs.items():
-        lines.append(f"Voxel Spacing: {spacing}")
-        for seg in seglist:
-            lines.append(f"\t{seg}")
+    for seg in segmentations:
+        lines.append(f"{seg}")
 
     if do_print:
         for line in lines:
-            print(line)
+            logger.info(line)
 
     return lines
 
@@ -59,21 +47,17 @@ def index_meshes(
     run: Union[str, CopickRun],
     do_print: bool = False,
 ) -> Union[List[str], None]:
-    """ "Index and optionally print the meshes associated with a run."""
+    """Index and optionally print the meshes associated with a run."""
 
-    if isinstance(run, str):
-        run = root.get_run(name=run)
-
-    meshes = run.meshes
+    meshes = get_meshes(root, run, parallel=True)
 
     lines = []
-
     for mesh in meshes:
         lines.append(f"{mesh}")
 
     if do_print:
         for line in lines:
-            print(line)
+            logger.info(line)
 
     return lines
 
@@ -85,19 +69,15 @@ def index_picks(
 ) -> Union[List[str], None]:
     """Index and optionally print the picks associated with a run."""
 
-    if isinstance(run, str):
-        run = root.get_run(name=run)
-
-    picks = run.picks
+    picks = get_picks(root, run, parallel=True)
 
     lines = []
-
     for pick in picks:
         lines.append(f"{pick}")
 
     if do_print:
         for line in lines:
-            print(line)
+            logger.info(line)
 
     return lines
 
@@ -110,17 +90,13 @@ def index_voxelspacings(
 ) -> Union[List[str], None]:
     """Index and optionally print the voxel spacings associated with a run."""
 
-    if isinstance(run, str):
-        run = root.get_run(name=run)
-
-    voxelspacings = run.voxel_spacings
+    vs = get_voxelspacings(root, run)
 
     lines = []
-
-    for voxelspacing in voxelspacings:
+    for voxelspacing in vs:
         lines.append(f"{voxelspacing}")
         if descend:
-            tlines = index_tomograms(root, run, voxelspacing, descend=True, do_print=False)
+            tlines = index_tomograms(root, run, voxelspacing, descend=descend, do_print=False)
             for line in tlines:
                 lines.append(f"\t{line}")
 
@@ -186,7 +162,6 @@ def index_run(
     run: CopickRun,
 ):
     """Print the contents of the run object."""
-    pass
 
 
 def index_root(
