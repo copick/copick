d68e0dd81b2343de5043a4dc19b4ddf236e4db7d
Kyle Harrington <czi@kyleharrington.com>
2024-12-08 12:59:42 -0500

    Add from_string to based module import

--- src/copick/__init__.py
+++ src/copick/__init__.py
@@ -1,9 +1,10 @@
 __version__ = "0.7.0"
 
-from copick.ops.open import from_czcdp_datasets, from_file
+from copick.ops.open import from_czcdp_datasets, from_file, from_string
 
 __all__ = [
     "from_file",
+    "from_string",
     "from_czcdp_datasets",
     "__version__",
 ]
