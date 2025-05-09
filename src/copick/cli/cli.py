44ae9fb773dd59a1420183b8dece5d72c68dcd7f
jtschwar <jtschwar@gmail.com>
2025-05-09 15:52:52 -0700

    Generate Config Files for the Dataportal Through CLI

--- src/copick/cli/cli.py
+++ src/copick/cli/cli.py
@@ -13,7 +13,7 @@ def cli(ctx):
     pass
 
 
-@cli.command()
+@cli.command(context_settings={"show_default": True})
 @click.option(
     "-c",
     "--config",
@@ -56,6 +56,40 @@ def browse(
         logger.critical("You must specify either a config or a dataset.")
         ctx.fail("Either --config or --dataset-ids must be provided.")
 
+@cli.command(context_settings={"show_default": True})
+@click.option(
+    '-ds',
+    '--dataset-id',
+    type=int, 
+    required=True, 
+    help="Dataset ID from the CryoET Data Portal to include in the configuration")
+@click.option(
+    '--overlay',
+    type=str, 
+    required=True, 
+    help='Path to the local overlay directory where intermediate files will be stored or read.')
+@click.option(
+    '--output', 
+    default='config.json',
+    type=str, 
+    required=False, 
+    help='Path to save the generated configuration file.')
+@click.pass_context
+def generate_dataportal_config(
+    ctx,
+    dataset_id: str,
+    overlay: str, 
+    output: str):
+    """
+    Generate a configuration file from a CZDP dataset ID and local overlay directory
+    """
+
+    # Generate Config for the Given Directory
+    copick.from_czcdp_datasets(
+        [dataset_id], 
+        overlay_root = overlay, 
+        output_path = output,
+        overlay_fs_args = {"auto_mkdir": True})
 
 if __name__ == "__main__":
     cli()
